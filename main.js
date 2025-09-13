2f4298a85252e5c1ff13360d3a0d677c0e6f0584
aklsal <aklsal.0818@gmail.com>
2025-09-12 23:52:50 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Question 1: alphabetString
+console.log(`--------------------------
+Question 1: alphabetString \n`);
+
+let alphabetString = 'abcdefghijklmnopqrstuvwxyz'; 
+
+/*
+ * Using the provided code above:
+ * Step 1: Convert the alphabetString to an array and print it to the console (See documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split)
+ * 
+ * 
+ * ↓ YOUR CODE HERE ↓ */
+
+//Split function
+
+
+
